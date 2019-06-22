7043c49e3e82ed66da83f069998aa707dc69790d
Aleksandr Zhyhaiev <test123@test.com>
2019-06-22 22:40:30 +0300

    123

--- jenkinsfiles/test2.groovy
+++ jenkinsfiles/test2.groovy
@@ -1,18 +1,20 @@
 node ('test2slave') {
-    def mvnHome = tool name: 'maven-3.5.4'
+    //def mvnHome = tool name: 'maven-3.0.5'
     stage ('GitHub Preparation') { 
         git 'https://github.com/spring-projects/spring-petclinic'
     }
     stage ('Maven clean + compile') {
-        withEnv(["PATH+MAVEN=${tool 'maven-3.5.4'}/bin"]) {
+       // withEnv(["PATH+MAVEN=${tool 'maven-3.5.4'}/bin"]) {
             sh 'mvn clean compile'
-        }
+        //}
     }
 
     stage ('Maven package') {
         // Run the maven build
-        withEnv(["PATH+MAVEN=${tool 'maven-3.5.4'}/bin"]) {
+        // withEnv(["PATH+MAVEN=${tool 'maven-3.5.4'}/bin"]) {
             sh 'mvn package'
-        }
+        // }
     }
 }
+
+
