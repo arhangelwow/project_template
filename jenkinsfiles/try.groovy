ecd21ef09cd41881e6c0c6c49b79f86ab69d8208
Aleksandr Zhyhaiev <test123@test.com>
2019-06-23 15:23:25 +0300

    try pipe

--- jenkinsfiles/try.groovy
+++ jenkinsfiles/try.groovy
@@ -5,7 +5,7 @@ node ('test2slave') {//node
   stage('Start Petclinic Website') {//stage
        try {//try
           sh 'cd /home/centos/workspace/maven_sh_pipeline/; mvn clean install;
-            \ cd /home/centos/workspace/maven_sh_pipeline/target; java -jar spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar'
+              cd /home/centos/workspace/maven_sh_pipeline/target; java -jar spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar'
        } catch (Exception err) {//catch
            currentBuild.result = 'SUCCESS'
        }//catch
