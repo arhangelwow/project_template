df45af68aab998b02e90dcb1a13004ecb49abfaa
arhangelwow <52069379+arhangelwow@users.noreply.github.com>
2020-01-09 16:24:05 +0200

    update path

--- jenkinsfiles/try.groovy
+++ jenkinsfiles/try.groovy
@@ -4,9 +4,9 @@ node ('Slave1Centos') {//node
   
   stage('Start Petclinic Website') {//stage
        try {//try
-          sh '''cd /home/centos/workspace/maven_sh++_pipeline/
+          sh '''cd /var/lib/jenkins/workspace/maven_sh++_pipeline/
                 mvn clean install
-                cd /home/centos/workspace/maven_sh++_pipeline/target
+                cd /var/lib/jenkins/workspace/maven_sh++_pipeline/target
                 java -jar spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar'''
        } catch (Exception err) {//catch
            currentBuild.result = 'SUCCESS'
