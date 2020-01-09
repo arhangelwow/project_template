83917845257fce72dd70d1916fe5b3e2b0b2241e
arhangelwow <52069379+arhangelwow@users.noreply.github.com>
2020-01-09 17:58:20 +0200

    add mvn skip tests

--- jenkinsfiles/try.groovy
+++ jenkinsfiles/try.groovy
@@ -6,7 +6,7 @@ def mvnHome = tool 'Maven_3.6.3'  //global variable for maven installations in j
     stage('Start Petclinic Website') {//stage 
             withEnv(["PATH+MAVEN=${mvnHome}/bin", "JENKINS_NODE_COOKIE=do_not_kill", "BUILD_ID=do_not_kill"]) {//withEnv
                 sh '''cd /home/centos/workspace/spring_petclinic_pipeline/
-                mvn clean install
+                mvn clean install -DskipTests
                 cd /home/centos/workspace/spring_petclinic_pipeline/target
                 nohup java -jar spring-petclinic-2.2.0.BUILD-SNAPSHOT.jar &'''
             }//withEnv
